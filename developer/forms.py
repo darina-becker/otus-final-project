d09fe9a0d216c2f13e0bf475f61612ffed901d2d
Daria Bukharina <daria.bukharina@gmail.com>
2022-05-28 15:45:37 +0300

    form fixes

--- developer/forms.py
+++ developer/forms.py
@@ -12,3 +12,8 @@ class DevAccountCreateForm(ModelForm):
         """Save the request with the form so it can be accessed in clean_*()"""
         self.request = kwargs.pop('request', None)
         super(DevAccountCreateForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs['style'] = 'margin-left: 1em;'
+            if name == 'email':
+                field.widget.attrs['style'] = 'margin-left: 3em;'
+
