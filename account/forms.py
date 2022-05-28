5b84b011760b320dd8a427068bba28301f3b70d6
Daria Bukharina <d.bukharina@yadro.com>
2022-05-28 12:57:50 +0300

    Some style

--- account/forms.py
+++ account/forms.py
@@ -8,4 +8,11 @@ class AccountCreateForm(UserCreationForm):
         model = Account
         fields = ('first_name', 'last_name', 'username', 'email', 'birth_date', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        for name, field in self.fields.items():
+            if name == 'email':
+                field.widget.attrs['style'] = 'margin-left: 3em;'
+            else:
+                field.widget.attrs['style'] = 'margin-left: 1em;'
